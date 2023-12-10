5b39756c9734583ffd34ff30eab63963800beca0
Zeeshan <115539434+azeegit@users.noreply.github.com>
2023-12-10 21:27:49 +0000

    with un typed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typed from "react-typed";
+
 
 
 const Hero = () => {
@@ -10,9 +10,7 @@ const Hero = () => {
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Invest in Yourself</h1>
                 <div className="flex justify-center items-center">
                     <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4"> Investing for : </p>
-                    <Typed 
-                    className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
-                    strings={["startups","Investors", " you"]} typeSpeed={120} backSpeed={140} loop></Typed>
+                    
                 </div>
                 <div>
                     <button className="bg-[#00df9a] w-[200px] rounded-md px-4 py-2 font-medium text-xl mt-4 text-black">Get Started</button>
